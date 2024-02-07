359357d11384555a1db7d22338962872fc5f039b
dilushaeka <dilushaeka99@gmail.com>
2024-02-08 01:11:11 +0530

    completed

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -1,8 +1,18 @@
+const router = require("express").Router();
+const { checkToken } = require("../../auth/token_validation");
+const {
+  createUser,
+  login,
+  getUserByUserId,
+  getUsers,
+  updateUsers,
+  deleteUser
+} = require("./user.controller");
+router.get("/", checkToken, getUsers);
+router.post("/", checkToken, createUser);
+router.get("/:id", checkToken, getUserByUserId);
+router.post("/login", login);
+router.patch("/", checkToken, updateUsers);
+router.delete("/", checkToken, deleteUser);
 
-const {createUser} = require("./user.controller")
-const { router } = require("express").Router();
-
-router.post("/", createUser);
-
-
-module.exports= router
+module.exports = router;
--- api/users/user.service.js
+++ api/users/user.service.js
@@ -1,4 +1,4 @@
-const pool = require("../../config/db");
+const pool = require("../../config/database");
 
 module.exports = {
   create: (data, callBack) => {
